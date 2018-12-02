3b6cb213df35773e67040b3244a9a0b3e5a6af0c
jun maeda <j.maeda@activeltd.co.jp>
2018-12-02 17:20:03 +0900

    .NET 3.5 版の作成

---
+++ DirectoryWatcherNet35/CommandParser.cs
@@ -0,0 +1,89 @@
+﻿namespace DirectoryWatcher
+{
+    public class CommandParser
+    {
+        /// <summary>
+        /// パラメータを処理する
+        /// </summary>
+        /// <param name="Arguments"></param>
+        public void Parse(string[] Arguments)
+        {
+            foreach (var param in Arguments)
+            {
+                if (string.IsNullOrEmpty(param))
+                    continue;
+                var p = param;
+                var IsFlag = false;
+                if (param.Length > 1 && (param[0] == '-' || param[0] == '/'))
+                {
+                    IsFlag = true;
+                    p = param.Substring(1);
+                }
+                ParseParam(p, IsFlag);
+            }
+        }
+        /// <summary>
+        /// ヘルプの要求
+        /// </summary>
+        public string Path { private set; get; } = null;
+        /// <summary>
+        /// サービスが起動していない場合はサービスを開始する
+        /// </summary>
+        public string Filter { private set; get; } = null;
+
+        public System.IO.NotifyFilters NotifyFilters = System.IO.NotifyFilters.LastAccess
+            | System.IO.NotifyFilters.LastWrite
+            | System.IO.NotifyFilters.FileName
+            | System.IO.NotifyFilters.DirectoryName;
+        /// <summary>
+        /// 次のコマンド処理モード定義
+        /// </summary>
+        protected enum NextCommandType
+        {
+            None = 0,
+            SetPath,
+            SetFilter,
+        }
+        /// <summary>
+        /// 次のコマンド処理モード
+        /// </summary>
+        protected NextCommandType NextCommand = default;
+
+        public CommandParser()
+        {
+        }
+
+        /// <summary>
+        /// パラメータの分解された値を処理する
+        /// </summary>
+        /// <param name="Param"></param>
+        /// <param name="IsFlag"></param>
+        protected void ParseParam(string Param, bool IsFlag)
+        {
+            if (NextCommand == NextCommandType.None || IsFlag)
+            {
+                NextCommand = NextCommandType.None;
+                if (Param[0] == 'P' || Param[0] == 'p')
+                {
+                    NextCommand = NextCommandType.SetPath;
+                }
+                else if (Param[0] == 'F' || Param[0] == 'F')
+                {
+                    NextCommand = NextCommandType.SetFilter;
+                }
+            }
+            else
+            {
+                switch (NextCommand)
+                {
+                    case NextCommandType.SetPath:
+                        Path = Param;
+                        break;
+                    case NextCommandType.SetFilter:
+                        Filter = Filter;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ DirectoryWatcherNet35/DirectoryWatcher.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DirectoryWatcher
+{
+    public class DirectoryWatcher : IDisposable
+    {
+        public string Path => Watcher?.Path;
+        public string Filter => Watcher?.Filter;
+        public System.IO.NotifyFilters NotifyFilters => Watcher?.NotifyFilter ?? default;
+        public System.ComponentModel.ISynchronizeInvoke SynchronizingObject => Watcher?.SynchronizingObject ?? default;
+        System.IO.FileSystemWatcher Watcher = null;
+        public DirectoryWatcher(Action<object, System.IO.FileSystemEventArgs> Changed = default, Action<object, System.IO.RenamedEventArgs> Renamed = default
+            , string Path = default, string Filter = default, System.IO.NotifyFilters NotifyFilters = default, System.ComponentModel.ISynchronizeInvoke SynchronizingObject = default) 
+            : this(Changed, Changed, Changed, Renamed, Path, Filter, NotifyFilters, SynchronizingObject){ }
+
+        public DirectoryWatcher(Action<object, System.IO.FileSystemEventArgs> Changed = default, Action<object, System.IO.FileSystemEventArgs> Created = default, Action<object, System.IO.FileSystemEventArgs> Deleted = default, Action<object, System.IO.RenamedEventArgs> Renamed = default
+            , string Path = default, string Filter = default, System.IO.NotifyFilters NotifyFilters = default, System.ComponentModel.ISynchronizeInvoke SynchronizingObject = default)
+        {
+            Watcher = new System.IO.FileSystemWatcher()
+            {
+                Path = Path,
+                Filter = Filter,
+                NotifyFilter = NotifyFilters,
+                SynchronizingObject = SynchronizingObject,
+            };
+            if (Changed != null)
+                Watcher.Changed += new System.IO.FileSystemEventHandler(Changed);
+            if (Created != null)
+                Watcher.Created += new System.IO.FileSystemEventHandler(Created);
+            if (Deleted != null)
+                Watcher.Deleted += new System.IO.FileSystemEventHandler(Deleted);
+            if (Renamed != null)
+                Watcher.Renamed += new System.IO.RenamedEventHandler(Renamed);
+            Watcher.EnableRaisingEvents = true;
+        }
+        public static Task StartAsync(CancellationToken Token, Action<object, System.IO.FileSystemEventArgs> Changed = default, Action<object, System.IO.RenamedEventArgs> Renamed = default
+            , string Path = default, string Filter = default, System.IO.NotifyFilters NotifyFilters = default, System.ComponentModel.ISynchronizeInvoke SynchronizingObject = default)
+            => StartAsync(Token, Changed, Changed, Changed, Renamed, Path, Filter, NotifyFilters, SynchronizingObject);
+        public static async Task StartAsync(CancellationToken Token, Action<object, System.IO.FileSystemEventArgs> Changed = default, Action<object, System.IO.FileSystemEventArgs> Created = default, Action<object, System.IO.FileSystemEventArgs> Deleted = default, Action<object, System.IO.RenamedEventArgs> Renamed = default
+            , string Path = default, string Filter = default, System.IO.NotifyFilters NotifyFilters = default, System.ComponentModel.ISynchronizeInvoke SynchronizingObject = default)
+        {
+            try
+            {
+                using (new DirectoryWatcher(Changed, Created, Deleted, Renamed, Path, Filter, NotifyFilters, SynchronizingObject))
+                    await Task.Delay(Timeout.Infinite, Token);
+            }
+            catch (ObjectDisposedException) { }
+            catch (TaskCanceledException) { }
+        }
+        #region IDisposable Support
+        private bool disposedValue = false; // 重複する呼び出しを検出するには
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if( Watcher is System.IO.FileSystemWatcher _Watcher)
+                    {
+                        _Watcher.EnableRaisingEvents = false;
+                        _Watcher.Dispose();
+                        Watcher = null;
+                    }
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、下のファイナライザーをオーバーライドします。
+                // TODO: 大きなフィールドを null に設定します。
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 上の Dispose(bool disposing) にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします。
+        // ~DirectoryWatcher() {
+        //   // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+        //   Dispose(false);
+        // }
+
+        // このコードは、破棄可能なパターンを正しく実装できるように追加されました。
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+            Dispose(true);
+            // TODO: 上のファイナライザーがオーバーライドされる場合は、次の行のコメントを解除してください。
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
