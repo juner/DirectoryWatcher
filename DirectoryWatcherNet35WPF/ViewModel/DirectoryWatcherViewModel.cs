037158c511002531e65a34f6a1739dd9f257febd
juner <juner@juner.net>
2018-12-06 08:34:17 +0900

    Net35WPF 版の追加途中

---
+++ DirectoryWatcherNet35WPF/ViewModel/DirectoryWatcherViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DirectoryWatcher.ViewModel
+{
+    class DirectoryWatcherViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void RaisePropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            if (PropertyName == null)
+                return;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
